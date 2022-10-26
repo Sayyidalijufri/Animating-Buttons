71b4da80601dc0cb6ae0083fe39340c14ee03d3d
Sayyid ali aljufri <88267951+Sayyidalijufri@users.noreply.github.com>
2022-10-26 12:26:04 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,20 @@ const App = () => {
       <Socials modeToggle={toggleMode} />
       <Main modeToggle={toggleMode} modeToggleFunc={setToggleMode} />
       <Footer />
+            <div className="loader-overlay"></div>
+      <div className="loader"></div>
     </div>
   );
 };
+
+const root = document.querySelector("body");
+
+root.classList.add("loading")
+
+
+setTimeout(() => {
+  root.classList.remove("loading")
+}, 3000);
+
 
 export default App;
